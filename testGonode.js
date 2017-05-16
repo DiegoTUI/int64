6458b41d0b978bf16729afc6167d59af6cf84757
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-16 16:46:51 +0200

    finished gonode

--- testGonode.js
+++ testGonode.js
@@ -4,6 +4,9 @@ const Go = require('gonode').Go;
 const assert = require('assert');
 
 const numbers = require('./numbers');
+
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
  
 const options = {
     path		: 'gonode.go',
@@ -17,51 +20,34 @@ const initGo = () => new Promise(resolve => {
     go = new Go(options, resolve);
 });
 
-const performOperation = (n1, n2, op, expected) => new Promise(resolve => {
-    go.execute({n1: n1, n2: n2, op: op}, function(result, response) {
-        console.log(response.response, expected);
-        assert.equal(response.response, expected, 'Failed: ' + n1 + ' - ' + n2 + ' - ' + op);
+const performSumOperation = (n1, n2, expected) => new Promise(resolve => {
+    go.execute({n1: n1, n2: n2, op: 'sum'}, function(result, response) {;
+        assert.equal(response.response, expected, 'Failed sum: ' + n1 + ' + ' + n2);
         return resolve();
     });
 });
 
-
-initGo()
-    .then(() => performOperation(numbers.number1, numbers.number2, 'sum', numbers.number1plus2))
-    .then(() => go.close())
-    .then(() => process.exit(0));
-
-
-const test = (n1, n2, op, expected) => {
-    return new Promise(resolve => {
-        const go = new Go(options, () => {
-            go.execute({n1: n1, n2: n2, op: op}, function(result, response) {
-                assert.equal(response.response, expected, 'Failed: ' + n1 + ' - ' + n2 + ' - ' + op);
-                
-                go.close();
-
-                return resolve();
-            });
-        });
+const performDivOperation = (n1, n2, expected) => new Promise(resolve => {
+    go.execute({n1: n1, n2: n2, op: 'div'}, function(result, response) {;
+        assert(Math.abs(response.response - expected) < numbers.TOLERANCE, 'Failed div: ' + n1 + ' + ' + n2);
+        return resolve();
     });
-}
+});
 
-/*test('123', '222', 'div', 0.5540540540540541)
-    .then(() => console.log('done'))*/
+const main = async(function() {
+    await(initGo())
+    for(let i = 0; i < numbers.NUM_ITERATIONS; i++) {
+        await(performSumOperation(numbers.number1, numbers.number2, numbers.number1plus2));
+        await(performSumOperation(numbers.number1, numbers.number3, numbers.number1plus3));
+        await(performSumOperation(numbers.number1, numbers.number4, numbers.number1plus4));
 
-
-/*const go2 = new Go(options, (err) => {
-    if (err) throw err;
- 
-    for(let i = 0; i < 100; i++) {
-        console.log(i)
-        go2.execute({n1: '123', n2: '222', op: 'div'}, function(result, response) {
-            if(result.ok) {
-                console.log('Go responded: ' + response.response);
-            }
-        });
+        await(performDivOperation(numbers.number1, numbers.number2, numbers.number1div2));
+        await(performDivOperation(numbers.number1, numbers.number3, numbers.number1div3));
+        await(performDivOperation(numbers.number1, numbers.number4, numbers.number1div4));
     }
-    
- 
-    go2.close();
-});*/
+});
+
+console.time('gonode lib');
+main()
+    .then(() => console.timeEnd('gonode lib'))
+    .then(() => process.exit(0));
