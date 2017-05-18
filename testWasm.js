20abd8b0bdbe66fa4e5c074e209eb8f083c5ae69
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-18 15:50:27 +0200

    refactorized and added performSumOperation

--- testWasm.js
+++ testWasm.js
@@ -1,22 +1,43 @@
 'use strict';
 
 const fs = require('fs');
+const assert = require('assert');
 const buf = fs.readFileSync('./addTwoInt64.wasm');
 const Int64 = require("int64-buffer").Int64LE
+const numbers = require('./numbers');
 
 const memory = new WebAssembly.Memory({initial:1});
 
-// `Wasm` does **not** understand node buffers, but thankfully a node buffer
-// is easy to convert to a native Uint8Array.
-function toUint8Array(buf) {
-  var u = new Uint8Array(buf.length);
-  for (var i = 0; i < buf.length; ++i) {
-    u[i] = buf[i];
-  }
-  return u;
+const n1Int64 = new Int64(numbers.number1, 10);
+const n2Int64 = new Int64(numbers.number2, 10);
+const n3Int64 = new Int64(numbers.number3, 10);
+const n4Int64 = new Int64(numbers.number4, 10);
+const n5Int64 = new Int64(numbers.number5, 10);
+const n6Int64 = new Int64(numbers.number6, 10);
+const n7Int64 = new Int64(numbers.number7, 10);
+
+let waspInstance;
+let waspInstanceMemoryUint32;
+
+const performSumOperation = (n1Int64, n2Int64, expected) => {
+    waspInstanceMemoryUint32[0] = n1Int64.toBuffer().readUInt32LE(0);
+    waspInstanceMemoryUint32[1] = n1Int64.toBuffer().readUInt32LE(4);
+    waspInstanceMemoryUint32[2] = n2Int64.toBuffer().readUInt32LE(0);
+    waspInstanceMemoryUint32[3] = n2Int64.toBuffer().readUInt32LE(4);
+    waspInstance.exports.addTwo();
+    assert.equal(new Int64(waspInstanceMemoryUint32.buffer, 16).toString(), expected,
+                            'Failed sum operation - ' + n1Int64.toString() + '-' +
+                                                        n1Int64.toString() + '-' +
+                                                        expected);
+}
+    
+
+const performDivOperation = (n1, n2, expected) => {
+    expectedResult = expected;
+    return client.write(n1 + '|' + n2 + '|div');
 }
 
-WebAssembly.compile(toUint8Array(buf))
+WebAssembly.compile(new Uint8Array(buf))
     .then(module => {
         const imports = {
             console: {
@@ -32,19 +53,17 @@ WebAssembly.compile(toUint8Array(buf))
         return new WebAssembly.Instance(module, imports);
     })
     .then(instance => {
+        waspInstance = instance;
         const n1 = '1223372036854775808';
         const n2 = '1523372036854775808';
         const n1Int64 = new Int64(n1, 10);
         const n2Int64 = new Int64(n2, 10);
-        const i32 = new Uint32Array(memory.buffer);
-        i32[0] = n1Int64.toBuffer().readUInt32LE(0);
-        i32[1] = n1Int64.toBuffer().readUInt32LE(4);
-        i32[2] = n2Int64.toBuffer().readUInt32LE(0);
-        i32[3] = n2Int64.toBuffer().readUInt32LE(4);
-        instance.exports.addTwo();
-        const result = new Int64(i32.buffer, 16);
-        console.log(i32[0], i32[1], i32[2], i32[3], i32[4], i32[5], i32[6], i32[7])
-        console.log(result.toString())
+        waspInstanceMemoryUint32 = new Uint32Array(memory.buffer);
+
+        for (let i = 0; i < 100000; i++) {
+            performSumOperation(n1Int64, n2Int64, numbers.number1plus2);
+        }
+
         process.exit(0);
     })
     .catch(console.log);
