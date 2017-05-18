7093e9e3a0d06cfe242cdc4b77bb9c0be544651c
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-18 12:06:51 +0200

    took the creation of the numbers out of the main loop

--- testBigNumber.js
+++ testBigNumber.js
@@ -6,18 +6,18 @@ const BigNumber = require('bignumber.js');
 
 const numbers = require('./numbers');
 
+const n1 = new BigNumber(numbers.number1);
+const n2 = new BigNumber(numbers.number2);
+const n3 = new BigNumber(numbers.number3);
+const n4 = new BigNumber(numbers.number4);
+const n5 = new BigNumber(numbers.number5);
+const n6 = new BigNumber(numbers.number6);
+const n7 = new BigNumber(numbers.number7);
+
 // test performance of bignumber lib
 console.time('bignumber lib');
 
 for (let i = 0; i < numbers.NUM_ITERATIONS; i++) {
-    const n1 = new BigNumber(numbers.number1);
-    const n2 = new BigNumber(numbers.number2);
-    const n3 = new BigNumber(numbers.number3);
-    const n4 = new BigNumber(numbers.number4);
-    const n5 = new BigNumber(numbers.number5);
-    const n6 = new BigNumber(numbers.number6);
-    const n7 = new BigNumber(numbers.number7);
-
     assert.equal(n1.plus(n2).toString(), numbers.number1plus2, 'n1 + n2 failed');
     assert.equal(n1.plus(n3).toString(), numbers.number1plus3, 'n1 + n3 failed');
     assert.equal(n1.plus(n4).toString(), numbers.number1plus4, 'n1 + n4 failed');
