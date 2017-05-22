3a0aeba0c79a9677691bb1b52f7d25b44470b3e4
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-22 16:42:18 +0200

    added some more numbers

--- numbers.js
+++ numbers.js
@@ -11,11 +11,17 @@ module.exports = {
     number5: '76254284',
     number6: '6352',
     number7: '71641941399',
+    number8: '26043322062',
+    number9: '392596353409127',
+    number10: '17076198',
 
     number1plus2: '2746744073709551616',
     number1plus3: '-500000000000000000',
     number1plus4: '100000000000000000',
     number1div5: '16043322062',
     number1div6: '192596353409127',
-    number1div7: '17076198'
+    number1div7: '17076198',
+    number5times8: '1985914876819213608',
+    number6times9: '2493772036854774704',
+    number7times10: '1223371976433721002'
 };
