c27ad5baa0933c1d9844016b077d9f34e598d89c
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-22 16:40:18 +0200

    added bits

--- int64.js
+++ int64.js
@@ -9,9 +9,22 @@ const Int64 = function(number) {
                     new Int64Buffer(number, 10) :
                     new Int64Buffer(number);
     this.buffer = this.model.toBuffer();
+    this.bits = this.toBits();
 };
 
 Int64.prototype = {
+
+    toBits: function() {
+        const resultBuffer = new Buffer(64);
+        for (let i = 0; i < 8; i++) {
+            const littelEndianBits = this.buffer[i].toString(2).split('').reverse()
+            for(let j = 0; j < 8; j++) {
+                resultBuffer[8*i + j] = littelEndianBits[j] || 0;
+            }
+        }
+
+        return resultBuffer;
+    },
 
     toString: function() {
         return this.model.toString();
@@ -31,6 +44,21 @@ Int64.prototype = {
         return new Int64(resultBuffer);
     },
 
+    plusBits: function(anotherInt64) {
+        const resultBuffer = new Buffer(8);
+        const bits1 = this.bits;
+        const bits2 = anotherInt64.bits;
+        const resultBits = new Array(64);
+        let carry = 0;
+        for (let i = 0; i < 64; i++) {
+            let res = bits1[i] + bits2[i] + carry;
+            carry = res > 1;
+            resultBits[i] = carry ? res - 2 : res;
+        }
+
+        return new Int64(bufferFromLEBits(resultBits));
+    },
+
     twosCompliment: function() {
         let bytes = this.buffer;
         let carry = 1;
@@ -42,6 +70,15 @@ Int64.prototype = {
         }
     }
 };
+
+function bufferFromLEBits(leBits) {
+    const resultBuffer = new Buffer(8);
+    for (let i = 0; i < 8; i++) {
+        resultBuffer[i] = parseInt(leBits.slice(8*i, 8*(i+1)).reverse().join(''), 2);
+    }
+
+    return resultBuffer;
+}
 
 module.exports = Int64;
 
