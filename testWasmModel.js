0e26bc67bc299b672e968772857c1591b1b916ed
Diego Lafuente Garcia <tufosa@gmail.com>
2017-05-27 15:11:01 +0200

    tests working

--- testWasmModel.js
+++ testWasmModel.js
@@ -16,8 +16,18 @@ const n5Int64buf = new Int64Buffer(numbers.number5, 10).toBuffer();
 const n6Int64buf = new Int64Buffer(numbers.number6, 10).toBuffer();
 const n7Int64buf = new Int64Buffer(numbers.number7, 10).toBuffer();
 
+const n1plusn2Int64buf = new Int64Buffer(numbers.number1plus2, 10).toBuffer();
+const n1plusn3Int64buf = new Int64Buffer(numbers.number1plus3, 10).toBuffer();
+const n1plusn4Int64buf = new Int64Buffer(numbers.number1plus4, 10).toBuffer();
+
+const n1divn5Int64buf = new Int64Buffer(numbers.number1div5, 10).toBuffer();
+const n1divn6Int64buf = new Int64Buffer(numbers.number1div6, 10).toBuffer();
+const n1divn7Int64buf = new Int64Buffer(numbers.number1div7, 10).toBuffer();
+
+
 let waspInstance;
 const waspInstanceMemoryUint32 = new Uint32Array(memory.buffer);
+const waspInstanceMemoryBuffer = new Buffer(memory.buffer);
 
 const Int64 = function(buffer, offset) {
     offset = offset || 0;
@@ -33,7 +43,7 @@ Int64.prototype = {
         waspInstanceMemoryUint32[2] = anotherInt64.low;
         waspInstanceMemoryUint32[3] = anotherInt64.high;
         waspInstance.exports.addTwo();
-        return new Int64(waspInstanceMemoryUint32.buffer, 16);
+        return new Int64(waspInstanceMemoryBuffer, 16);
     },
 
     dividedBy: function(anotherInt64) {
@@ -42,7 +52,15 @@ Int64.prototype = {
         waspInstanceMemoryUint32[2] = anotherInt64.low;
         waspInstanceMemoryUint32[3] = anotherInt64.high;
         waspInstance.exports.divTwo();
-        return new Int64(waspInstanceMemoryUint32.buffer, 16);
+        return new Int64(waspInstanceMemoryBuffer, 16);
+    },
+
+    equals: function(anotherInt64) {
+        return (this.low === anotherInt64.low) && (this.high === anotherInt64.high);
+    },
+
+    toString: function() {
+        return new Int64Buffer(this.high, this.low).toString();
     }
 }
 
@@ -50,6 +68,17 @@ const n1Int64 = new Int64(n1Int64buf);
 const n2Int64 = new Int64(n2Int64buf);
 const n3Int64 = new Int64(n3Int64buf);
 const n4Int64 = new Int64(n4Int64buf);
+const n5Int64 = new Int64(n5Int64buf);
+const n6Int64 = new Int64(n6Int64buf);
+const n7Int64 = new Int64(n7Int64buf);
+
+const n1plusn2Int64 = new Int64(n1plusn2Int64buf);
+const n1plusn3Int64 = new Int64(n1plusn3Int64buf);
+const n1plusn4Int64 = new Int64(n1plusn4Int64buf);
+
+const n1divn5Int64 = new Int64(n1divn5Int64buf);
+const n1divn6Int64 = new Int64(n1divn6Int64buf);
+const n1divn7Int64 = new Int64(n1divn7Int64buf);
 
 WebAssembly.compile(new Uint8Array(buf))
     .then(module => {
@@ -69,15 +98,19 @@ WebAssembly.compile(new Uint8Array(buf))
     .then(instance => {
         waspInstance = instance;
 
+        assert(n1Int64.plus(n2Int64).equals(n1plusn2Int64));
+        assert(n1Int64.plus(n3Int64).equals(n1plusn3Int64));
+        assert(n1Int64.plus(n4Int64).equals(n1plusn4Int64));
+
         console.time('testWasp');
-        for (let i = 0; i < 100000; i++) {
+        for (let i = 0; i < numbers.NUM_ITERATIONS; i++) {
             n1Int64.plus(n2Int64);
             n1Int64.plus(n3Int64);
             n1Int64.plus(n4Int64);
 
-            //performDivOperation(n1Int64, n5Int64, numbers.number1div5);
-            //performDivOperation(n1Int64, n6Int64, numbers.number1div6);
-            //performDivOperation(n1Int64, n7Int64, numbers.number1div7);
+            n1Int64.dividedBy(n5Int64);
+            n1Int64.dividedBy(n6Int64);
+            n1Int64.dividedBy(n7Int64);
         }
         console.timeEnd('testWasp');
 
